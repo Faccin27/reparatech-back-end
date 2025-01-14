c263855a93a7ec8a1b554a6954277eb08035ad95
Guilherme Faccin <gfaccin27@gmail.com>
2025-01-14 16:22:59 -0300

    feat: Created the new routes to redirect the user to the google oAuth Page and a callback URL to get the OAuth acess token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, HttpException, Logger, Res, UseGuards } from "@nestjs/common";
+import { GoogleStrategy } from "./auth.google.stategy";
+import { GoogleAuth } from "./guards/auth.google.guard";
+import { Response } from "express";
 
-@Controller()
-export class AuthController{};
+@Controller("auth")
+@UseGuards(GoogleAuth)
+export class AuthController{
+    private readonly logger = new Logger(AuthController.name);
+    constructor(private readonly googleStreategy:GoogleStrategy){};
+
+    @Get("/google/token")
+    private async getOAuthAcessToken(){};
+
+    @Get(process.env.GOOGLE_CALLBACK_URL1)
+    private async callBackURL(@Res()res:Response):Promise<Response>{
+        try{
+            const token = await this.googleStreategy.validate();
+
+            return res.status(202).send(token)
+        } catch(err){
+            this.logger.error(`${err.message}`);
+            throw new HttpException({server:`${err.message}`},err.status);
+        };
+    };
+};
