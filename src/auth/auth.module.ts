2a3047063d52e265b84a95deb45779d64c532e45
Guilherme Faccin <gfaccin27@gmail.com>
2025-01-14 16:06:18 -0300

    congif: Make the Google Guard a provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { GoogleAuth } from "./guards/auth.google.guard";
 
 @Module({
     imports:[],
     controllers:[AuthController],
-    providers:[AuthService],
+    providers:[AuthService,GoogleAuth],
 })
 export class AuthModule{};
