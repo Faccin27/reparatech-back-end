4683ce11afa767bb58a6cf5740266eb8c893b3f6
Guilherme Faccin <gfaccin27@gmail.com>
2025-01-14 16:17:00 -0300

    feat: Added new method to get the auth acesstoken

---
+++ src/auth/auth.google.stategy.ts
@@ -0,0 +1,29 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import {Strategy,VerifyCallback} from "passport-google-oauth20"
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy,"google"){
+    private readonly logger = new Logger(GoogleStrategy.name);
+
+    constructor(){
+        super({
+            clientID:process.env.GOOGLE_CLIENT_ID,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL:process.env.GOOGLE_CALLBACK_URL1,
+            scope:["email","profile"],
+        });
+    };
+
+    public async validate(accessToken?:string, refreshToken?:string, profile?:any, done?:VerifyCallback):Promise<any>{
+
+        const userInfo = {
+            token:accessToken
+        };
+
+        this.logger.debug(userInfo)
+        
+        done(null,userInfo);
+    };
+
+};
