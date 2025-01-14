b53a92a69a345946b3ceba26340d2202d48cf019
Guilherme Faccin <gfaccin27@gmail.com>
2025-01-14 16:17:53 -0300

    config: Maked the GoogleStrategy a provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { GoogleAuth } from "./guards/auth.google.guard";
+import { GoogleStrategy } from "./auth.google.stategy";
 
 @Module({
     imports:[],
     controllers:[AuthController],
-    providers:[AuthService,GoogleAuth],
+    providers:[AuthService,GoogleAuth,GoogleStrategy],
 })
 export class AuthModule{};
