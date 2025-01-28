160c158ac4d82b4250ee9bbec7bcee0443be714b
Guilherme Faccin <gfaccin27@gmail.com>
2025-01-28 15:29:46 -0300

    feat: Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,22 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 
-
 dotenv.config();
 
 async function bootstrap() {
-  const logger = new Logger();
-  const app = await NestFactory.create(AppModule,{cors:true});
-  app.enableCors();
-  let port = 7865;
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 7865;
   await app.listen(port);
 
-  logger.warn(`API Running at: http://localhost:${port}`);
-
+  logger.warn(`Application running on  http://localhost:${port}`);
 }
 bootstrap();
