d131912723c8586c3022206b7165892d3c8fb4b4
Guilherme Faccin <gfaccin27@gmail.com>
2025-01-28 15:54:22 -0300

    config: Configurated the .env for the vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
-    AuthModule
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // disponível em toda aplicação
+      envFilePath: '.env',
+    }),
   ],
   controllers: [],
   providers: [],
