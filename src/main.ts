8ba5ed0a338822443a9448f38cf33965fee0b1b3
Guilherme Faccin <gfaccin27@gmail.com>
2025-01-29 22:27:25 -0300

    refactored: Make tge API listen in some port

--- src/main.ts
+++ src/main.ts
@@ -9,20 +9,10 @@ import { Context, Handler } from 'aws-lambda';
 const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
-  await app.init();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  await app.listen(7865);
 }
 
 bootstrap();
-
-const server = createServer(expressApp);
-
-export const handler: Handler = (event: any, context: Context) => {
-  return proxy(server, event, context);
-};
