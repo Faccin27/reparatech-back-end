f46a2eb44c4c067a4fd002efb7a196f383bec911
Guilherme Faccin <gfaccin27@gmail.com>
2025-01-29 23:34:53 -0300

    Meu nome é Faccin e eu sou gay

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
-import { createServer, proxy } from 'aws-serverless-express';
-import { Context, Handler } from 'aws-lambda';
-
-const expressApp = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  await app.listen(7865);
+  await app.listen(process.env.PORT ?? 7865);
 }
 
 bootstrap();
